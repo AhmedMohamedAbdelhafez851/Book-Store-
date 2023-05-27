64cc0cb173a6784b24d8df0c543dcb03534a01e0
AhmedMohamedAbdelhafez851 <102626727+AhmedMohamedAbdelhafez851@users.noreply.github.com>
2023-05-27 14:05:31 +0300

    Add files via upload

---
+++ Views/Shared/_Item.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book1.Views.Shared
+{
+    public class _ItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
